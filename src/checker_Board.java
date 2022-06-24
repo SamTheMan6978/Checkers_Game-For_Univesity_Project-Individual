bc79c9cc2563698559cb9b809be30956697ff884
ss7460 <ss7460@uowmail.edu.au>
2022-06-24 11:37:43 +0400

    Amending Logic

--- src/checker_Board.java
+++ src/checker_Board.java
@@ -33,39 +33,5 @@ public class checker_Board {
             }
             System.out.print(checkers_Board_OG[x][y]);
         }
-
-        System.out.print("\n");
-
-        for (int i = 0; i < checkers_Board_OG.length; i++)
-            for (int j = 0; j < checkers_Board_OG[i].length; j++) {
-
-                randomizer_Counter = (int) (Math.random() * 3);
-
-                if (checkers_Board_OG[i][j] == '.') {
-                    if (randomizer_Counter == 0)
-                        checkers_Board_OG[i][j] = white_Pieces[(int) (Math.random() * 2)];
-                    else if (randomizer_Counter == 1)
-                        continue;
-                    else
-                        checkers_Board_OG[i][j] = black_Pieces[(int) (Math.random() * 2)];
-                }
-
-                if (checkers_Board_OG[i][7] == 'b' || checkers_Board_OG[i][7] == 'B') {
-                    checkers_Board_OG[i][7] = '.';
-                }
-
-                if (checkers_Board_OG[i][0] == 'w' || checkers_Board_OG[i][0] == 'W') {
-                    checkers_Board_OG[i][0] = '.';
-                }
-            }
-
-        for (int i = 0; i < 64; i++) {
-            int x = i % 8;
-            int y = (i - x) / 8;
-            if (i % 8 == 0) {
-                System.out.println();
-            }
-            System.out.print(checkers_Board_OG[x][y]);
-        }
     }
 }
